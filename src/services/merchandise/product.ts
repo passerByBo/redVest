3aa154e4fff070de69e7fe4a144d5412979c65ec
shaoli <shaolisxxa@qq.com>
2021-03-17 09:19:25 +0800

    增加商品管理service

--- src/services/merchandise/product.ts
+++ src/services/merchandise/product.ts
@@ -1,16 +1,85 @@
 import { request } from 'umi';
+
 export async function getProductList(
   params: {
     current?: number;
     pageSize?: number;
   },
   options?: { [key: string]: any },
-){
+) {
   return request('/product/info/list', {
     method: 'GET',
     params: {
       ...params,
     },
-    ...(options || {})
-  })
+    ...(options || {}),
+  });
+}
+
+/** 商品类型列表 */
+export async function getProductTypeList(
+  params: {
+    current?: number;
+    pageSize?: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request('/product/type/list', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** 品牌类型列表 */
+export async function getBrandList(
+  params: {
+    current?: number;
+    pageSize?: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request('/product/brand/list', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** 品牌类型列表 */
+export async function getSpecialList(
+  params: {
+    current?: number;
+    pageSize?: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request('/product/special/list', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** 品牌类型列表 */
+export async function getSpecialGroupList(
+  params: {
+    current?: number;
+    pageSize?: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request('/product/specialGroup/list', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
 }
