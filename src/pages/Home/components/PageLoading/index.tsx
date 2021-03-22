67a4b09155b4abc65a6a524e7a2f9be8c95afb46
shixiaobo <sxb5626@163.com>
2021-03-22 15:53:53 +0800

    修改首页

--- src/pages/Home/components/PageLoading/index.tsx
+++ src/pages/Home/components/PageLoading/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Spin } from 'antd';
+import {Spin} from 'antd';
 
-// loading components from code split
-// https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
+
 export default () => (
-  <div style={{ paddingTop: 100, textAlign: 'center' }}>
-    <Spin size="large" />
+  <div style={{paddingTop:100, textAlign: 'center'}}>
+    <Spin size='large'/>
   </div>
-);
+)
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,218 +1,24 @@
+import React, { Suspense } from 'react';
 import { EllipsisOutlined } from '@ant-design/icons';
 import { Col, Dropdown, Menu, Row } from 'antd';
-import React, { Component } from 'react';
+//等宽和流式布局
 import { GridContent } from '@ant-design/pro-layout';
-import type { RadioChangeEvent } from 'antd/es/radio';
-import type { RangePickerProps } from 'antd/es/date-picker/generatePicker';
-import type moment from 'moment';
-import type { Dispatch } from 'umi';
-import { connect } from 'umi';
-import IntroduceRow from './components/IntroduceRow';
-import SalesCard from './components/SalesCard';
-import TopSearch from './components/TopSearch';
-import ProportionSales from './components/ProportionSales';
-import OfflineData from './components/OfflineData';
-
-import { getTimeDistance } from './utils/utils';
-import type { AnalysisData } from './data.d';
+import PageLoading from './components/PageLoading'
 import styles from './style.less';
 
-type RangePickerValue = RangePickerProps<moment.Moment>['value'];
 
-type HomeProps = {
-  home: AnalysisData;
-  dispatch: Dispatch;
-  loading: boolean;
-};
+const IntroduceRow = React.lazy(() => import('./components/IntroduceRow'))
 
-type HomeState = {
-  salesType: 'all' | 'online' | 'stores';
-  currentTabKey: string;
-  rangePickerValue: RangePickerValue;
-};
-
-class Home extends Component<HomeProps, HomeState> {
-  state: HomeState = {
-    salesType: 'all',
-    currentTabKey: '',
-    rangePickerValue: getTimeDistance('year'),
-  };
-
-  reqRef: number = 0;
-
-  timeoutId: number = 0;
-
-  componentDidMount() {
-    const { dispatch } = this.props;
-    this.reqRef = requestAnimationFrame(() => {
-      dispatch({
-        type: 'home/fetch',
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'home/clear',
-    });
-    cancelAnimationFrame(this.reqRef);
-    clearTimeout(this.timeoutId);
-  }
-
-  handleChangeSalesType = (e: RadioChangeEvent) => {
-    this.setState({
-      salesType: e.target.value,
-    });
-  };
-
-  handleTabChange = (key: string) => {
-    this.setState({
-      currentTabKey: key,
-    });
-  };
-
-  handleRangePickerChange = (rangePickerValue: RangePickerValue) => {
-    const { dispatch } = this.props;
-    this.setState({
-      rangePickerValue,
-    });
-
-    dispatch({
-      type: 'home/fetchSalesData',
-    });
-  };
-
-  selectDate = (type: 'today' | 'week' | 'month' | 'year') => {
-    const { dispatch } = this.props;
-    this.setState({
-      rangePickerValue: getTimeDistance(type),
-    });
-
-    dispatch({
-      type: 'home/fetchSalesData',
-    });
-  };
-
-  isActive = (type: 'today' | 'week' | 'month' | 'year') => {
-    const { rangePickerValue } = this.state;
-    if (!rangePickerValue) {
-      return '';
-    }
-    const value = getTimeDistance(type);
-    if (!value) {
-      return '';
-    }
-    if (!rangePickerValue[0] || !rangePickerValue[1]) {
-      return '';
-    }
-    if (
-      rangePickerValue[0].isSame(value[0] as moment.Moment, 'day') &&
-      rangePickerValue[1].isSame(value[1] as moment.Moment, 'day')
-    ) {
-      return styles.currentDate;
-    }
-    return '';
-  };
-
-  render() {
-    const { rangePickerValue, salesType, currentTabKey } = this.state;
-    const { home, loading } = this.props;
-    const {
-      visitData,
-      visitData2,
-      salesData,
-      searchData,
-      offlineData,
-      offlineChartData,
-      salesTypeData,
-      salesTypeDataOnline,
-      salesTypeDataOffline,
-    } = home;
-    let salesPieData;
-    if (salesType === 'all') {
-      salesPieData = salesTypeData;
-    } else {
-      salesPieData = salesType === 'online' ? salesTypeDataOnline : salesTypeDataOffline;
-    }
-    const menu = (
-      <Menu>
-        <Menu.Item>操作一</Menu.Item>
-        <Menu.Item>操作二</Menu.Item>
-      </Menu>
-    );
-
-    const dropdownGroup = (
-      <span className={styles.iconGroup}>
-        <Dropdown overlay={menu} placement="bottomRight">
-          <EllipsisOutlined />
-        </Dropdown>
-      </span>
-    );
-
-    const activeKey = currentTabKey || (offlineData[0] && offlineData[0].name);
-    return (
-      <GridContent>
-        <React.Fragment>
-          <IntroduceRow loading={loading} visitData={visitData} />
-          <SalesCard
-            rangePickerValue={rangePickerValue}
-            salesData={salesData}
-            isActive={this.isActive}
-            handleRangePickerChange={this.handleRangePickerChange}
-            loading={loading}
-            selectDate={this.selectDate}
-          />
-          <Row
-            gutter={24}
-            style={
-              {
-                //  marginTop: 24,
-              }
-            }
-          >
-            <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-              <TopSearch
-                loading={loading}
-                visitData2={visitData2}
-                searchData={searchData}
-                dropdownGroup={dropdownGroup}
-              />
-            </Col>
-            <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-              <ProportionSales
-                dropdownGroup={dropdownGroup}
-                salesType={salesType}
-                loading={loading}
-                salesPieData={salesPieData}
-                handleChangeSalesType={this.handleChangeSalesType}
-              />
-            </Col>
-          </Row>
-          <OfflineData
-            activeKey={activeKey}
-            loading={loading}
-            offlineData={offlineData}
-            offlineChartData={offlineChartData}
-            handleTabChange={this.handleTabChange}
-          />
-        </React.Fragment>
-      </GridContent>
-    );
-  }
+const Home: React.FC = () => {
+  return (
+    <GridContent>
+      <>
+        <Suspense fallback={<PageLoading />}>
+          <IntroduceRow loading={loading} visitData={visitData}/>
+        </Suspense>
+      </>
+    </GridContent>
+  )
 }
 
-export default connect(
-  ({
-    home,
-    loading,
-  }: {
-    home: any;
-    loading: {
-      effects: Record<string, boolean>;
-    };
-  }) => ({
-    home,
-    loading: loading.effects['home/fetch'],
-  }),
-)(Home);
+export default Home;
