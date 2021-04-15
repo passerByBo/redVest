f5356fbb0d44962cba4524a79367a8c220eb9a3b
shaoli <shaolisxxa@qq.com>
2021-04-15 10:09:15 +0800

    coupon

--- src/pages/Marketing/Coupon/CouponProduction/index.tsx
+++ src/pages/Marketing/Coupon/CouponProduction/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { PlusOutlined } from '@ant-design/icons';
+import { PlusOutlined, DeleteOutlined, ExportOutlined } from '@ant-design/icons';
 import { EllipsisOutlined } from '@ant-design/icons';
 import { Button, Dropdown, Menu } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
@@ -7,9 +7,39 @@ import ProCard from '@ant-design/pro-card';
 import ProTable, { TableDropdown } from '@ant-design/pro-table';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
 import { rule, addRule, updateRule, removeRule } from '@/services/ant-design-pro/rule';
+
+
 const CouponProduction: React.FC = () => {
 
+
     const actionRef = useRef<ActionType>();
+
+    const onTabChange = (key: string) => {
+        console.log(key);
+    };
+
+    const toolBarRenderList = [
+        (
+            <Button type="primary" key="primary" onClick={() => { }}>
+                <PlusOutlined />新建
+            </Button>
+        ), (
+            <Button key="delete" danger onClick={() => { }}>
+                <DeleteOutlined />删除
+            </Button>
+        ), (
+            <Button key="export" onClick={() => { }}>
+                <ExportOutlined />导出
+            </Button>
+        )
+    ]
+
+
+
+
+
+
+
 
     const columns: ProColumns<API.RuleListItem>[] = [
         {
@@ -32,11 +62,13 @@ const CouponProduction: React.FC = () => {
             title: '使用类型',
             dataIndex: 'name',
             valueType: 'textarea',
+            search: false,
         },
         {
             title: '卡券数量',
             dataIndex: 'status',
             valueType: 'textarea',
+            search: false,
         },
         {
             title: '单一卡券金额',
@@ -45,29 +77,52 @@ const CouponProduction: React.FC = () => {
             search: false,
         },
         {
-            title: '采购总金额(小写)',
+            title: '采购总金额',
             dataIndex: 'status',
             valueType: 'textarea',
+            search: false,
+        },
+        {
+            title: '时效状态',
+            dataIndex: 'status',
+            valueType: 'textarea',
+        },
+        {
+            title: '审核状态',
+            dataIndex: 'status',
+            valueType: 'textarea',
+            search: false,
         },
         {
             title: '有效期开始日期',
             dataIndex: 'createdAt',
             valueType: 'textarea',
+            search: false,
         },
         {
             title: '有效期结束日期',
             dataIndex: 'createdAt',
             valueType: 'textarea',
+            search: false,
         },
         {
-            title: '维护时间',
+            title: '创建人',
             dataIndex: 'createdAt',
             valueType: 'textarea',
         },
         {
-            title: '维护人',
-            dataIndex: 'name',
+            title: '创建时间',
+            dataIndex: 'createdAt',
             valueType: 'textarea',
+        },
+        {
+            title: '操作',
+            dataIndex: 'option',
+            valueType: 'option',
+            render: (_, record) => [
+                <a onClick={() => { }}>编辑</a>,
+                <a onClick={() => { }}>下架</a>
+            ],
         },
     ]
     return (
@@ -75,24 +130,29 @@ const CouponProduction: React.FC = () => {
             header={{
                 title: '卡券制作',
             }}
+            tabList={
+                [
+                    {
+                        tab: '审核通过卡券',
+                        key: 'passed'
+                    },
+                    {
+                        tab: '审核未通过卡券',
+                        key: 'denied'
+                    }
+                ]
+            }
+            onTabChange={onTabChange}
         >
             <ProTable<API.RuleListItem, API.PageParams>
                 headerTitle="卡券制作"
                 actionRef={actionRef}
                 rowKey="key"
                 search={{
-                    labelWidth: 120,
+                    labelWidth: 100,
+                    defaultCollapsed: false
                 }}
-                toolBarRender={() => [
-                    <Button
-                        type="primary"
-                        key="primary"
-                        onClick={() => {
-                        }}
-                    >
-                        <PlusOutlined /> 新建
-          </Button>,
-                ]}
+                toolbar={{ actions: toolBarRenderList }}
                 request={rule}
                 columns={columns}
                 rowSelection={{
